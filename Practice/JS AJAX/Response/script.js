43607b06f6018d4131126e1326a99a12ac6fc93c
shahriar <shahriarkabir078@gmail.com>
2023-05-03 23:17:10 +0600

    new

--- Practice/JS AJAX/Response/script.js
+++ Practice/JS AJAX/Response/script.js
@@ -1,4 +1,3 @@
-
 const display = document.querySelector("#btn");
 display.addEventListener('click',()=>{
   getData("../data/data.txt");
@@ -8,11 +7,12 @@ display.addEventListener('click',()=>{
     //create a new request 
     const xhr = new XMLHttpRequest();
     //what to do when response arrives
-    xhr.onload = () =>{
-     btn.innerHTML = xhr.responseText;
+    xhr.onload = function() {
+     btn.innerHTML =this.responseText;
+     console.log(this.getAllResponseHeaders());
     }
     //prepare request 
-    xhr.open("GET", url); //3rd parameter as false means syncronous
+    xhr.open("GET", url);   //3rd parameter as false means syncronous
     //request send          //use random value so that it doesn't provide cashed value
     xhr.send();
 }
