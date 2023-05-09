ee3c4ac62fd9ef6dde8929d92ef4034f6faf81d3
shahriar <shahriarkabir078@gmail.com>
2023-05-09 23:25:16 +0600

    new

--- Practice/JS AJAX/Multiple_httpRequest/script.js
+++ Practice/JS AJAX/Multiple_httpRequest/script.js
@@ -10,20 +10,47 @@ function sendRequest(method,url,data){
   const promise = new Promise(function(resolve, reject){
   
     const xhr = new XMLHttpRequest();
+
+    
     xhr.onload = () => {
-     callbackFunction(xhr);
+      if(xhr.status>=400){
+        reject(`There was an application error and the error status is ${xhr.status}`);
+      }
+      else{
+        resolve(xhr.response);
+      }
+     
     }
-    xhr.open(method, url);
-    xhr.send();
+
+    xhr.onerror = () => {
+      reject("There was an error");
+    }  
+   xhr.open(method, url);
+    xhr.send(data);
 
   });
     return promise;
 }
 
 function getData(){
-  sendRequest("GET","https://jsonplaceholder.typicode.com/todos/test");
+  sendRequest("GET","https://jsonplaceholder.typicode.com/pots/1")
+  .then(res=>{console.log(res)})
+  .catch(err => console.log(err));
 }
 function sendData() {
+  sendRequest("POST", "https://jsonplaceholder.typicode.com/posts",{
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then(res =>  console.log(res))
+  .catch(err => console.log(err));
 
   
 }
