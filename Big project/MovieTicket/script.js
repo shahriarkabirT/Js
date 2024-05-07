a71fb5be89e1dabf08c1c43a62986e350ab06407
shahriar <shahriarkabir078@gmail.com>
2024-05-07 11:38:50 +0600

    new

---
+++ Big project/MovieTicket/script.js
@@ -0,0 +1,14 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    if (username === "shahriar" && password === "123") {
+      window.location.href = "s.html"; // Replace "another-page.html" with the URL of the page you want to redirect to upon successful login.
+    } else {
+      const errorMessage = document.getElementById("error-message");
+      errorMessage.textContent = "Invalid username or password.";
+    }
+  });
+  
---
+++ Topics of JS/Basics/method_chaining.js
@@ -0,0 +1,4 @@
+let firstName = "  Rajat  ";
+console.log(firstName);
+let modifiedName = firstName.toUpperCase().trim();
+console.log(modifiedName);
